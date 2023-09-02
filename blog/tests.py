e561d4c559e19d9013e9df6e86bfc6865878b049
RSP Procesů <vyacheslavusmanov@gmail.com>
2023-09-02 10:28:43 +0000

    Finish security ad passwords

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django import forms
+
+from blog.models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
